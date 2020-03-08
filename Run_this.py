d8f047ea10fda599fbab5cd0b920d5cacfc78752
TakanashiKoucha <katanashikoucha@outlook.com>
2020-03-08 22:21:35 +0800

    可用性修复与调整

--- Run_this.py
+++ Run_this.py
@@ -1,10 +1,13 @@
 # -*- coding: UTF-8 -*-
 # @LastAuthor: TakanashiKoucha
-# @Date: 2019-12-29 20:23:25
+# @Date: 2020-03-08 04:54:00
 import re
+import time
 
 import requests
 from lxml import etree
+
+starttime = time.time()
 
 # 最终列表
 Zh_list = []
@@ -13,21 +16,28 @@ Ja_list = []
 # 地址列表
 url_list = []
 
-# 进行遍历循环（当前最大页数为10）
+# 进行遍历循环（最大页数为10,每页20条）
+s_get = requests.Session()
 for page_num in range(1, 11):
     page_num = str(page_num)
-    url = "http://j.people.com.cn/95961/index" + page_num + ".html"
-    r = requests.get(url)
+    pageurl = "http://j.people.com.cn/95961/index" + page_num + ".html"
+    r = s_get.get(pageurl)
     r.encoding = "utf-8"
     page_html = r.text
     paser = etree.HTML(page_html)
-    url_list.append(paser.xpath("/html/body/div[6]/div[1]/div[3]//@href"))
+    for i in range(20):
+        url_list.append(
+            paser.xpath("/html/body/div[5]/div[1]/div[3]/div[" + str(i + 1) +
+                        "]/h3/a/@href"))
+        print("已添加路径数：" + str(len(url_list)))
+    time.sleep(0.1)
+    print("翻页等待0.1s")
 print("地址获取完成")
 
 
 # 分句函数
 def gen_split(sents):
-    sents_list = re.split('(。|！|\!|？|\?)', sents)
+    sents_list = re.split(r"(。|！|\!|？|\?)", sents)
     new_sents = []
     for i in range(int(len(sents_list) / 2)):
         sent = sents_list[2 * i] + sents_list[2 * i + 1]
@@ -35,9 +45,12 @@ def gen_split(sents):
     return new_sents
 
 
+s_download = requests.Session()
+
+
 # 下载函数
 def download(url):
-    r = requests.get("http://j.people.com.cn" + url)
+    r = s_download.get("http://j.people.com.cn" + url[0])
     r.encoding = "utf-8"
     page_html = r.text
     paser = etree.HTML(page_html)
@@ -56,9 +69,7 @@ def download(url):
 url_head = "http://j.people.com.cn"
 for index, item in enumerate(url_list):
     print("Page:  " + str(index + 1))
-    for indey, url in enumerate(item):
-        print("Num:  " + str(indey + 1))
-        download(url)
+    download(item)
 
 # 写入文件
 with open("zh.txt", "a", encoding="utf-8") as f:
@@ -69,3 +80,7 @@ with open("ja.txt", "a", encoding="utf-8") as f:
     for ja in Ja_list:
         f.writelines(ja + "\n")
     print("日语写入完成")
+
+
+endtime = time.time()
+print("程序执行时间: ", endtime - starttime)
